2b28dbf4f6fea7f98f4f596394463dc7c8483b0b
Agustin Villalobos <avillaville@gmail.com>
2023-08-29 19:38:18 +0200

    Adding token types

---
+++ src/monkey/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier and literals
+	IDENTIFIER = "IDENTIFIER" // add, foo, bar, x, y
+	INT        = "INT"        //1234
+
+	//Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
